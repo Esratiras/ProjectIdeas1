2fe81860ef8a20e5a6361795b84ff9aaa696ab39
feyzayayci <rfeyzayayci@gmail.com>
2022-09-15 00:48:03 +0300

    test

--- note-taking-app/src/router/general.js
+++ note-taking-app/src/router/general.js
@@ -1,9 +1,9 @@
 const router = require("express").Router()
-const login =require("../controller/Login")
-const getNotes =require("../controller/GetNotes")
+const login = require("../controller/Login")
+const getNotes = require("../controller/GetNotes")
 
-router.get("/login",login)
-router.get("/all-notes",getNotes)
+router.get("/login", login)
+router.get("/all-notes", getNotes)
 
 
 module.exports = router
